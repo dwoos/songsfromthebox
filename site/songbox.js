2d8b2cbd003a36736d4c717251a1a9716ff15bdc
Doug Woos <doug.woos@gmail.com>
2024-02-06 22:34:53 -0800

    controls

--- site/songbox.js
+++ site/songbox.js
@@ -1,4 +1,14 @@
-const toggleMenu = () => $(".sidenav-contents").toggle();
+const toggleMenu = () => {
+    const menuContents = $(".sidenav-contents");
+    if (menuContents.is(":visible")) {
+        menuContents.hide();
+    } else {
+        menuContents.show();
+        $(".song-search").val("");
+        $(".song-search").trigger("focus");
+        $(".song-search").triggerHandler("input");
+    }
+};
 const button = $(".sidenav-button a");
 button.click(toggleMenu);
 
@@ -14,6 +24,8 @@ const parsedSongs = songs.map((song) => {
 var displayedSong;
 
 const displaySong = (song) => {
+    console.log("displaying song");
+    console.log(song);
     const formatter = new ChordSheetJS.HtmlDivFormatter();
     const disp = formatter.format(song);
     $("#song").html(disp);
@@ -40,11 +52,12 @@ const displaySong = (song) => {
 
 parsedSongs.forEach((song) => {
     const item = $(`<a href="#${song.slug}">${song.song.title}</a>`);
-    item.click(() => {
+    item.on("click", () => {
+        console.log("hello");
         displaySong(song.song);
         toggleMenu();
     });
-    $(".sidenav-contents").append(item);
+    $(".sidenav-songs").append(item);
 });
 
 displaySong(parsedSongs[0].song);
@@ -68,3 +81,24 @@ $("body").on("keypress", (evt) => {
             break;
     }
 });
+
+$(".song-search").on("input", () => {
+    const search = $(".song-search").val();
+    $(".sidenav-songs a").each((_, el) => {
+        el = $(el);
+        if (
+            search === "" ||
+            el.text().toLowerCase().includes(search.toLowerCase())
+        ) {
+            el.show();
+        } else {
+            el.hide();
+        }
+    });
+});
+
+/*$(".song-search").on("keypress", (evt) => {
+    if (evt.keyCode === 13) {
+        $(".sidenav-songs a:visible").first().trigger("click");
+    }
+});*/
