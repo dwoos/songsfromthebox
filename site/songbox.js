36f82bf74e701ef66f6ef18e4a97c68e39bd1dee
Max Willsey <me@mwillsey.com>
2024-03-28 10:24:10 -0700

    Do a new menu

--- site/songbox.js
+++ site/songbox.js
@@ -1,44 +1,16 @@
-const hideMenu = () => {
-    $(".sidenav-contents").hide();
-};
+var SONG;
 
-const showMenu = () => {
-    $(".sidenav-contents").show();
-    $(".song-search").val("");
-    $(".song-search").trigger("focus");
-    $(".song-search").triggerHandler("input");
-};
-
-const toggleMenu = () => {
-    if ($(".sidenav-contents").is(":visible")) {
-        hideMenu();
-    } else {
-        showMenu();
-    }
-};
-const button = $(".sidenav-button a");
-button.on("click", (evt) => {
-    console.log("button clicked");
-    toggleMenu();
-    evt.preventDefault();
-});
-
-$(() => {
-    $(".sidenav-contents").hide();
-});
-
-var displayedSong;
-
-const displaySong = (song) => {
+function displaySong(song) {
     console.log("displaying song");
     console.log(song);
+    window.location.hash = song.slug;
     const formatter = new ChordSheetJS.HtmlDivFormatter();
     const disp = formatter.format(song);
     $("#song").html(disp);
-    displayedSong = song;
+    SONG = song;
     // get chords
     const chords = new Set();
-    displayedSong.lines.forEach((line) => {
+    SONG.lines.forEach((line) => {
         line.items.forEach((item) => {
             if (
                 item instanceof ChordSheetJS.ChordLyricsPair &&
@@ -48,60 +20,155 @@ const displaySong = (song) => {
             }
         });
     });
+    $("#title").text(song.title);
+    document.title = `Songbox: ${song.title}`;
     $(".footer").html("");
     chords.forEach((chord) => {
         const el = $("<div/>");
         $(".footer").append(el);
         jtab.render(el, chord);
     });
+}
+
+// Select all text in a contenteditable non-input element
+// For some reason just `.select()` doesn't work on non-inputs
+// https://stackoverflow.com/questions/12243898/how-to-select-all-text-in-contenteditable-div
+jQuery.fn.selectText = function () {
+    var doc = document;
+    var element = this[0];
+    console.log(this, element);
+    if (doc.body.createTextRange) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
 };
 
 $(() => {
     const parser = new ChordSheetJS.ChordProParser();
     const parsedSongs = songs.map((song) => {
-        return { slug: song.slug, song: parser.parse(song.song) };
+        let parsed = parser.parse(song.song);
+        parsed.slug = song.slug;
+        return parsed
     });
 
-    parsedSongs.sort((song1, song2) => {
-        if (song1.song.title < song2.song.title) {
-            return -1;
-        } else {
-            return 1;
+    parsedSongs.sort((song1, song2) => song1.title.localeCompare(song2.title));
+
+    function doSearch() {
+        $("#title")
+            .autocomplete("search", "")
+            .selectText()
+    }
+
+    $("#title")
+        .autocomplete({
+            delay: 50,
+            minLength: 0,
+            autoFocus: true,
+            source: [
+                {
+                    label: "Action: Shuffle",
+                    value: "Shuffle",
+                    action: function () {
+                        const song = parsedSongs[Math.floor(Math.random() * parsedSongs.length)];
+                        displaySong(song);
+                    }
+                },
+                {
+                    label: "Action: Transpose Up [+]",
+                    action: transposeUp,
+                },
+                {
+                    label: "Action: Transpose Down [-]",
+                    action: transposeDown,
+                },
+                {
+                    label: "Action: Add/Edit Song",
+                    action: function () {
+                        let url = "https://github.com/dwoos/songsfromthebox/tree/main";
+                        url += SONG ? `/${SONG.slug}` : "/songs"
+                        // window.open(url, '_blank').focus(); // new tab
+                        window.location = url;
+                    },
+                }
+            ].concat(
+                parsedSongs.map(song => ({
+                    label: `Song: ${song.title}`,
+                    value: song.title,
+                    action: function () {
+                        displaySong(song);
+                    }
+                }))
+            ),
+            select: function (event, ui) {
+                event.preventDefault();
+                this.blur();
+                ui.item.action();
+            }
+        })
+        .click(doSearch)
+        .keydown(function (e) {
+            console.log("keydown", e.keyCode);
+            // enter key
+            if (e.keyCode == 13) {
+                e.preventDefault();
+                this.blur();
+            }
+            // escape key
+            if (e.keyCode == 27) {
+                this.blur();
+            }
+        })
+        .on("blur", function () {
+            console.log("blur");
+            if (SONG) {
+                $(this).text(SONG.title)
+            }
+        });
+
+    // slash key does search
+    $(document).keydown(function (e) {
+        if (e.keyCode == 191) {
+            e.preventDefault();
+            doSearch();
         }
     });
 
-    parsedSongs.forEach((song) => {
-        const item = $(`<a href="#${song.slug}">${song.song.title}</a>`);
-        item.on("click", () => {
-            console.log("hello");
-            displaySong(song.song);
-            hideMenu();
-        });
-        $(".sidenav-songs").append(item);
-    });
-    $(".sidenav-contents").hide();
-    if (window.location.hash && window.location.hash !== "#") {
-        const slug = window.location.hash.substring(1);
-        console.log(slug);
-        parsedSongs.forEach((song) => {
-            if (song.slug === slug) {
-                displaySong(song.song);
+    function loadFromHash() {
+        const hash = window.location.hash;
+        if (hash) {
+            const song = parsedSongs.find(song => `#${song.slug}` == hash);
+            if (song) {
+                displaySong(song);
             }
-        });
-    } else {
-        $(() => $(".sidenav-controls .shuffle").trigger("click"));
+        }
     }
-});
-const transposeUp = () => {
-    const song = displayedSong.transposeUp(1);
-    displaySong(song);
-};
-const transposeDown = () => {
-    const song = displayedSong.transposeDown(1);
-    displaySong(song);
-};
 
-$("body").on("keypress", (evt) => {
+    window.onpopstate = loadFromHash;
+    loadFromHash();
+});
+
+
+function transposeUp() {
+    let slug = SONG.slug;
+    SONG = SONG.transposeUp(1);
+    SONG.slug = slug;
+    displaySong(SONG);
+}
+function transposeDown() {
+    let slug = SONG.slug;
+    SONG = SONG.transposeDown(1);
+    SONG.slug = slug;
+    displaySong(SONG);
+}
+
+$(document).on("keypress", (evt) => {
     switch (evt.keyCode) {
         case 45:
             transposeDown();
@@ -111,31 +178,3 @@ $("body").on("keypress", (evt) => {
             break;
     }
 });
-
-$(".song-search").on("input", () => {
-    const search = $(".song-search").val();
-    $(".sidenav-songs a").each((_, el) => {
-        el = $(el);
-        if (
-            search === "" ||
-            el.text().toLowerCase().includes(search.toLowerCase())
-        ) {
-            el.show();
-        } else {
-            el.hide();
-        }
-    });
-});
-
-$(".sidenav-controls .shuffle").on("click", (evt) => {
-    evt.preventDefault();
-    const songs = $(".sidenav-songs a");
-    const i = Math.floor(Math.random() * songs.length);
-    songs[i].click();
-});
-
-/*$(".song-search").on("keypress", (evt) => {
-    if (evt.keyCode === 13) {
-        $(".sidenav-songs a:visible").first().trigger("click");
-    }
-});*/
